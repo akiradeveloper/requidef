37dea8684b356cf0904d31e7ffb2d310901e0efc
Akira Hayakawa <ruby.wktk@gmail.com>
2011-08-22 13:00:08 +0900

    implemented html (from XMind) to .rd file translator.

---
+++ lib/html2tree.rb
@@ -0,0 +1,38 @@
+require "hpricot"
+
+def html2rd(html)
+  body = read_body(html)
+  regex = /<h(\d).*?>(.*?)<\/h\d>/m
+  body.scan(regex).map do |e|
+    hx2rd(e)
+  end.join("\n")
+end
+
+def hx2rd(header)
+  depth = header[0].to_i
+  s = read_anchor(header[1].strip)
+  "#{repeat(depth, "-")} #{s}"
+end
+
+def read_anchor(anchor)
+  anchor.scan(/<a.*?>(.*?)<\/a>/m)[0][0]
+end
+
+def repeat(n, s)
+  Array.new(n, s).join
+end
+
+def read_body(html)
+  doc = Hpricot(open("sample.html"))
+  x = ""
+  (doc/"body").each do |e|
+    x += e.to_html
+  end
+  x
+end
+
+if __FILE__ == $0
+  p html2rd("sample.html")
+  # p read_body("sample.html")
+  # p read_anchor("<a name=\"hoge\">text</a>")
+end
