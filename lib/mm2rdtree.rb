a8bbe1e8e39f5edaa4e5d9949274edf04a143dfa
Akira Hayakawa <ruby.wktk@gmail.com>
2011-08-27 00:51:52 +0900

    killed a bug in mm2retree.rb

--- lib/mm2rdtree.rb
+++ lib/mm2rdtree.rb
@@ -13,40 +13,50 @@ end
 # Doc -> Tree (of Text node class)
 def build_tree(mm)
   t = Tree.new
-  doc_root_id = 1
   t.add_node(0, DummyRoot)
-  t.add_edge(0, doc_root_id)
-  do_build_tree(root_node_of(mm), doc_root_id, 1, t) 
+  root = root_node_of(mm)
+  t.add_edge(0, root.attributes["ID"])
+  do_build_tree(root, 1, t) 
   t
 end
 
-def do_build_tree(doc, id, depth, t)
+def do_build_tree(doc, depth, t)
+  id = doc.attributes["ID"]
   t.add_node( id, to_node(doc, depth) )
-  n = doc.elements.size
-  for i in 1...n+1
-    child_id = id+i # TODO: Killing a bug here.
+
+  doc.elements.each("arrowlink") do |link|
+    # TODO: Implement 
+    # t.add_edge( id, child_id )
+  end  
+
+  doc.elements.each("node") do |child_doc|
     child_depth = depth + 1
-    child_doc = doc.elements[i]
+    child_id = child_doc.attributes["ID"]
     child_value = to_node( child_doc, child_depth )
     t.add_node( child_id, child_value )
     t.add_edge( id, child_id )
 
-    do_build_tree( child_doc, child_id, child_depth, t )
+    do_build_tree( child_doc, child_depth, t )
   end
 end
 
 def to_node(doc, depth)
   text = doc.attributes["TEXT"]
-  Text.new(depth, text)
+  tag = doc.attributes["ID"]
+  Tag.new(depth, tag, text)
 end
 
 def root_node_of(mm)
   doc = REXML::Document.new( mm )
-  doc.root.elements[1]
+  xs = []
+  doc.root.elements.each("node") do |node| 
+    xs << node
+  end
+  raise "Invalid mm file." unless xs.size == 1
+  xs[0]
 end
 
 if __FILE__ == $0
-  # p root_node_of("sample.mm")
   p mm2tree( File.read("sample.mm") )
 
   puts "# Test ------------------------"
